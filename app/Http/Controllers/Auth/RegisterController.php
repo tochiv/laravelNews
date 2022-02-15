952af8ce7978196e0f0d9e4fb599c279b1603626
tochiv <tochivk@gmail.com>
2022-02-15 17:40:35 +0300

    add user avatars

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\Models\User;
+use http\Client\Request;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -64,11 +65,18 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        $avatar = request()->file('avatar');
+
+        $avatarName = $avatar->hashName();
+
+        $avatar->store('uploads', 'public');
+
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-            'role_id' => 1
+            'role_id' => 1,
+            'avatar' => $avatarName
         ]);
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,7 @@ class User extends Authenticatable
         'email',
         'role_id',
         'password',
+        'avatar'
     ];
 
     /**
